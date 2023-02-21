858afc95f2c851c964f21366ed08cab85f7494f7
SarahAllison92 <109143210+SarahAllison92@users.noreply.github.com>
2023-02-21 14:39:36 +0000

    created the if else statements to manipulate the HTML to change the message and game score

--- src/script.js
+++ src/script.js
@@ -6,6 +6,29 @@ let score = 20;
 function onClick() {
   const guess = Number(document.querySelector("#number-input").value);
   console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector("#msg").textContent = "No number ...";
+  } else if (guess === secretNumber) {
+    document.querySelector("#msg").textContent = "Yey you guessed correct";
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector("#msg").textContent = "Sorry, That's too high ...";
+      score--;
+      document.querySelector("#score-num").textContent = score;
+    } else {
+      document.querySelector("#msg").textContent = "You lose the game, Sorry";
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector("#msg").textContent = "Sorry, That's too low ...";
+      score--;
+      document.querySelector("#score-num").textContent = score;
+    } else {
+      document.querySelector("#msg").textContent = "You lose the game, Sorry";
+    }
+  }
 }
+
 const clickBtn1 = document.querySelector("#check-btn");
 clickBtn1.addEventListener("click", onClick);
