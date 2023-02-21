5ba1b93dbcf73b1126913ad1ca516a8c296ef811
SarahAllison92 <109143210+SarahAllison92@users.noreply.github.com>
2023-02-21 15:17:19 +0000

    tidied up code and added frequently used query selectors into variables

--- src/script.js
+++ src/script.js
@@ -1,6 +1,5 @@
 "use strict";
 
-
 const secretNumber = Math.round(Math.random() * 30);
 let score = 20;
 document.querySelector("#number").textContent = secretNumber;
@@ -9,25 +8,41 @@ function onClick() {
   const guess = Number(document.querySelector("#number-input").value);
   console.log(guess, typeof guess);
 
+  let msg = document.querySelector("#msg");
+  let scoreNum = document.querySelector("#score-num");
+  let head = document.querySelector(".heading");
+  let check = document.querySelector("#check-btn");
+  let again = document.querySelector("#again-btn");
+  let h1 = document.querySelector("h1");
+  let h2 = document.querySelector("h2");
+
   if (!guess) {
-    document.querySelector("#msg").textContent = "No number ...";
+    msg.textContent = "No number ...";
   } else if (guess === secretNumber) {
-    document.querySelector("#msg").textContent = "Yey you guessed correct";
+    msg.textContent = "Yey you guessed correct";
+    head.style.backgroundColor = "#6EFDA5";
+    h1.style.color = "#000";
+    h2.style.color = "#000";
+    head.style.color = "#000";
+    check.style.backgroundColor = "#6EFDA5";
+    check.style.color = "#000";
+    again.style.backgroundColor = "#6EFDA5";
+    again.style.color = "#000";
   } else if (guess > secretNumber) {
     if (score > 1) {
-      document.querySelector("#msg").textContent = "Sorry, That's too high ...";
+      msg.textContent = "Sorry, That's too high ...";
       score--;
-      document.querySelector("#score-num").textContent = score;
+      scoreNum.textContent = score;
     } else {
-      document.querySelector("#msg").textContent = "You lose the game, Sorry";
+      msg.textContent = "You lose the game, Sorry";
     }
   } else if (guess < secretNumber) {
     if (score > 1) {
-      document.querySelector("#msg").textContent = "Sorry, That's too low ...";
+      msg.textContent = "Sorry, That's too low ...";
       score--;
-      document.querySelector("#score-num").textContent = score;
+      scoreNum.textContent = score;
     } else {
-      document.querySelector("#msg").textContent = "You lose the game, Sorry";
+      msg.textContent = "You lose the game, Sorry";
     }
   }
 }
