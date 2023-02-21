5a7a76c1bc1d6fb62643f89039e24d290af098d1
SarahAllison92 <109143210+SarahAllison92@users.noreply.github.com>
2023-02-21 14:37:06 +0000

    Created function to retrieve the value of the input

--- src/script.js
+++ src/script.js
@@ -1,5 +1,9 @@
 "use strict";
 
 console.log(document.querySelector(".score"));
+function onClick() {
+  const guess = Number(document.querySelector("#number-input").value);
+  console.log(guess, typeof guess);
+}
 const clickBtn1 = document.querySelector("#check-btn");
 clickBtn1.addEventListener("click", onClick);
