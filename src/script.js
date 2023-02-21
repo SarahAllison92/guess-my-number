1a62dd698e3b079a44684958258e1cf60b42d9af
SarahAllison92 <109143210+SarahAllison92@users.noreply.github.com>
2023-02-21 14:38:12 +0000

    Created the variables storing the random secret number and the score

--- src/script.js
+++ src/script.js
@@ -1,6 +1,8 @@
 "use strict";
 
 console.log(document.querySelector(".score"));
+const secretNumber = Math.round(Math.random() * 30);
+let score = 20;
 function onClick() {
   const guess = Number(document.querySelector("#number-input").value);
   console.log(guess, typeof guess);
