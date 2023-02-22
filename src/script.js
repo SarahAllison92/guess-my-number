b9565629cdaab4e1894be8a5905febafda4590c7
SarahAllison92 <109143210+SarahAllison92@users.noreply.github.com>
2023-02-22 15:11:55 +0000

    Changed the pervious HTML button click refresh to javascript

--- src/script.js
+++ src/script.js
@@ -1,19 +1,21 @@
 "use strict";
 
-const secretNumber = Math.round(Math.random() * 20);
+let secretNumber = Math.round(Math.random() * 20 + 1);
 let score = 20;
+let highscore = 0;
+
+let msg = document.querySelector("#msg");
+let scoreNum = document.querySelector("#score-num");
+let head = document.querySelector(".heading");
+let check = document.querySelector("#check-btn");
+let again = document.querySelector("#again-btn");
+let h1 = document.querySelector("h1");
+let h2 = document.querySelector("h2");
+let number = document.querySelector("#number");
+let input = document.querySelector("#number-input");
 
 function onClick1() {
   const guess = Number(document.querySelector("#number-input").value);
-  console.log(guess, typeof guess);
-
-  let msg = document.querySelector("#msg");
-  let scoreNum = document.querySelector("#score-num");
-  let head = document.querySelector(".heading");
-  let check = document.querySelector("#check-btn");
-  let again = document.querySelector("#again-btn");
-  let h1 = document.querySelector("h1");
-  let h2 = document.querySelector("h2");
 
   if (!guess) {
     msg.textContent = "No number ...";
@@ -28,7 +30,12 @@ function onClick1() {
     again.style.backgroundColor = "#6EFDA5";
     again.style.color = "#000";
 
-    document.querySelector("#number").textContent = secretNumber;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector("#high-score-num").textContent = highscore;
+    }
+
+    number.textContent = secretNumber;
   } else if (guess > secretNumber) {
     if (score > 1) {
       msg.textContent = "Sorry, That's too high ...";
@@ -56,5 +63,26 @@ function onClick1() {
   }
 }
 
+function onClick2() {
+  score = 20;
+  secretNumber = Math.round(Math.random() * 20 + 1);
+
+  msg.textContent = "Start Guessing....";
+  scoreNum.textContent = "20";
+  number.textContent = "?";
+  input.textContent = "";
+
+  head.style.backgroundColor = "#464B55";
+  h1.style.color = "#fff";
+  h2.style.color = "#fff";
+  check.style.backgroundColor = "rgb(70, 75, 85, 0.5)";
+  check.style.color = "#fff";
+  again.style.backgroundColor = "rgb(70, 75, 85, 0.5)";
+  again.style.color = "#fff";
+}
+
 const clickBtn1 = document.querySelector("#check-btn");
 clickBtn1.addEventListener("click", onClick1);
+
+const clickbtn2 = document.querySelector("#again-btn");
+clickbtn2.addEventListener("click", onClick2);
